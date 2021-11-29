45170ae2717642cd8dc8fa64051596b9a9a33ff5
JeB <jebbacca@gmail.com>
2021-11-29 10:19:49 +0200

    add chance

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
 import { List } from './List';
-const entries = Array.from({length: 200}, () => `${Math.floor(Math.random()*2000)}`);
+import { Chance } from 'chance';
+
+const chance = new Chance();
+
+const items = Array.from({length: 200}, () => `${chance.integer()}`);
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   return (
     <div className="App">
       <input type="text" onChange={(event) => setSearchTerm(event.target.value)}/>
-      <List entries={entries.filter(entry => entry.includes(searchTerm))}/>
+      <List entries={items.filter(item => item.includes(searchTerm))}/>
     </div>
   );
 }
