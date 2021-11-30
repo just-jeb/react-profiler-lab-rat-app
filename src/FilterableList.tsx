2912c4dc707ac2367bdca31950549b3deb371467
JeB <jebbacca@gmail.com>
2021-11-30 17:12:37 +0200

    fix after merge

--- src/FilterableList.tsx
+++ src/FilterableList.tsx
@@ -3,11 +3,11 @@ import { List } from './List';
 import { Chance } from 'chance';
 const chance = new Chance();
 
-const items = Array.from({ length: 200 }, () => `${chance.integer()}`);
+const items = Array.from({ length: 200 }, (_, index) => ({ value: `${chance.integer()}`, id: index}));
 export const FilterableList = () => {
     const [searchTerm, setSearchTerm] = useState('');
     return <>
         <input type="text" onChange={(event) => setSearchTerm(event.target.value)} />
-        <List entries={items.filter(item => item.includes(searchTerm))} />
+        <List entries={items.filter(item => item.value.includes(searchTerm))} />
     </>
 }
