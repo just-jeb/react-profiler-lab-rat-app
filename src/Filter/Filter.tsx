8f506d0a6c58d94b2e958bf37bbec0c466ae8174
JeB <jebbacca@gmail.com>
2021-12-09 14:26:05 +0200

    fixes

--- src/Filter/Filter.tsx
+++ src/Filter/Filter.tsx
@@ -8,5 +8,5 @@ interface FilterProps {
 export const Filter: FC<FilterProps> = ({ onValueUpdated }) =>
 (<div className="form__group field">
     <input type="input" name={'filter'} className={'form__field'} placeholder={'filter'} id={'filter'} onChange={(event) => onValueUpdated(event.target.value)}/>
-    <label htmlFor="filter" className="form__label">Enter number</label>
+    <label htmlFor="filter" className="form__label">Enter a number</label>
 </div>)
